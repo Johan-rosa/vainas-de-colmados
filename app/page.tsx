fc906905c7df3eef2b6802af4f95b115f02cecd2
Johan Rosa <johan.rosaperez@gmail.com>
2025-04-07 19:43:29 -0400

    chore: init shadcn and use orange theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+"use client"
+
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div>
+    <div className="p-4">
       <h1>Hello App</h1>
+      <Button onClick={() => setCount(count + 1)}>{`Clics (${count})`}</Button>
     </div>
   );
 }
