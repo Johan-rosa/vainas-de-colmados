e192c6d8c4c8dc4e2c751ff7a7928ed195ba2fcf
Johan Rosa <johan.rosaperez@gmail.com>
2025-04-11 20:00:48 -0400

    feat: implement balance loading and update navigation to balances list

--- app/registrar-balance/page.tsx
+++ app/registrar-balance/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react'
+import Link from 'next/link'
 
 import { PageHeader } from '@/components/page-header'
 import SelectColmado from "@/components/select-colmado"
@@ -9,10 +10,30 @@ import { Button } from "@/components/ui/button"
 import {ArrowLeft } from "lucide-react"
 
 import { ColmadoKey } from '@/types'
+import { getBalances } from '@/services/balances-service'
+import type { Balance } from '@/types'
 
 export default function RegisterBalance() {
   const [colmados, setColmados] = useState<{ key: string; name: string; balanceDate: number }[]>([])
   const [colmado, setColmado] = useState<ColmadoKey>("o7")
+  const [balances, setBalances] = useState<Balance[]>()
+
+  useEffect(() => {
+    const loadBalances = async () => {
+      try {
+        const balances = await getBalances(colmado, 36)
+        setBalances(balances)
+      } catch (e) {
+        if (e instanceof Error) {
+          console.error(e.message)
+        } else {
+          console.error("An unknown error occurred")
+        }
+      }
+    }
+
+    loadBalances()
+  }, [colmado])
 
   useEffect(() => {
     const loadColmados = async () => {
@@ -31,15 +52,19 @@ export default function RegisterBalance() {
       <div className="flex w-full items-center justify-between">
         <h2 className="text-xl">Registrar balances</h2>
         <div className="hidden md:block">
-          <SelectColmado selected={colmado} setSelected={(value: ColmadoKey) => setColmado(value)} />
+          <SelectColmado 
+            selected={colmado} 
+            setSelected={(value: ColmadoKey) => setColmado(value)}
+          />
         </div>
       </div>
       </PageHeader>
-
-      <Button variant="ghost" className='w-full flex justify-start'>
-        <ArrowLeft />
-        Registrar balance
-      </Button>
+      <Link href="/balances">
+        <Button variant="ghost" className='w-full flex justify-start'>
+          <ArrowLeft />
+          Lista de balances
+        </Button>
+      </Link>
     </> 
   )
 }
