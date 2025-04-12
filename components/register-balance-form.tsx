6cbaad21487d8352746da6b158933285cb994167
Johan Rosa <johan.rosaperez@gmail.com>
2025-04-12 08:59:14 -0400

    feat: init balances form

--- components/register-balance-form.tsx
+++ components/register-balance-form.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { useState } from "react"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
@@ -15,48 +16,37 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import {Card, CardContent} from "@/components/ui/card"
+import { useEffect } from "react"
 
 const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
+
 })
 
 export default function RegisterBlanceForm() {
-    // 1. Define your form.
+    const [isSubmitting, setIsSubmitting] = useState(false)
     const form = useForm<z.infer<typeof formSchema>>({
       resolver: zodResolver(formSchema),
-      defaultValues: {
-        username: "",
-      },
     })
 
     function onSubmit(values: z.infer<typeof formSchema>) {
+      setIsSubmitting(true)
       console.log(values)
+      setIsSubmitting(false)
     }
   
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+      <Card>
+        <CardContent className="pt-6">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+              <Button type="submit" className="w-full" disabled={isSubmitting}>
+                {isSubmitting ? "Registrando..." : "Registrar Balance"}
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
   )
 }
